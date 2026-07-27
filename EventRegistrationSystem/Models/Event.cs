488a791fcf7b93457bb66da60335d29326723f90
justin3435-324234 <manzanojustin37@gmail.com>
2026-07-28 01:41:06 +0800

    Add files via upload

---
+++ EventRegistrationSystem/Models/Event.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventRegistrationSystem.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string EventName { get; set; }
+
+        [Required]
+        public string Organizer { get; set; }
+
+        [Required]
+        public DateTime EventDate { get; set; }
+
+        [Required]
+        public string Venue { get; set; }
+
+        [Range(1, 10000)]
+        public int Participants { get; set; }
+    }
+}
