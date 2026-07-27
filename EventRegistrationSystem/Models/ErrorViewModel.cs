df9ddcc2e36b3364ab5090aa68ca4b1a958676de
kykie-hub <kykie1008@gmail.com>
2026-07-28 00:17:13 +0800

    Delete EventRegistrationSystem directory

--- EventRegistrationSystem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EventRegistrationSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
