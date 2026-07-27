c7ed33fae46d1cffda37a895edafe6ef1efbaaf2
justin3435-324234 <manzanojustin37@gmail.com>
2026-07-28 01:37:47 +0800

    Add files via upload

---
+++ EventRegistrationSystem/Controllers/EventController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EventRegistrationSystem.Models;
+
+namespace EventRegistrationSystem.Controllers
+{
+    public class EventController : Controller
+    {
+        private static List<Event> events = new List<Event>();
+
+        public IActionResult Index()
+        {
+            return View(events);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Event newEvent)
+        {
+            if (ModelState.IsValid)
+            {
+                newEvent.Id = events.Count + 1;
+                events.Add(newEvent);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(newEvent);
+        }
+    }
+}
